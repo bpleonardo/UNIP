4c2d281c165cbb25b91ad87c44f4deb31b992a5f
Leonardo <37600692+uKaigo@users.noreply.github.com>
2025-03-24 20:28:16 -0300

    Update lista1.py

--- PLCP/lista1.py
+++ PLCP/lista1.py
@@ -78,3 +78,43 @@ print('-=-= Exercício 9 (pares.py) =-=-')
 n = int(input('Digite um número: '))
 for cont in range(2, n, 2):
     print(cont)
+
+print()
+print('-=-= Exercício 10 (fatorial.py) =-=-')
+
+n = int(input('Digite um número: '))
+produto = 1
+for cont in range(n, 1, -1):
+    produto *= cont
+print(produto)
+
+print()
+print('-=-= Exercício 11 (fibonacci.py) =-=-')
+
+n = int(input('Digite um número: '))
+anter = 0
+atual = 1
+print(anter)
+print(atual)
+for _ in range(n):
+    prox = anter + atual
+    print(prox)
+    anter = atual
+    atual = prox
+
+print()
+print('-=-= Exercício 12 (primos.py) =-=-')
+
+n = int(input('Digite um número: '))
+soma = 0
+# Elevar um número a meio é o mesmo que tirar a raiz quadrada.
+for i in range(1, n + 1):
+    if n % i == 0:
+        soma += 1
+
+if soma == 2:
+    print('É primo!')
+else:
+    print('Não é primo!')
+    
+print()
