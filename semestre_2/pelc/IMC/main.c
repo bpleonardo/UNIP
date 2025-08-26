08e1d38e5efb5d165d309985edbb0765bdfe2e5b
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2025-08-26 09:48:50 -0300

    Utilizar funções

--- semestre_2/pelc/IMC/main.c
+++ semestre_2/pelc/IMC/main.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    float peso, altura;
+float leia_float(char* prompt) {
+    printf(prompt);
 
-    printf("Digite seu peso (kg): ");
-    scanf("%f", &peso);
+    float resultado;
+    scanf("%f", &resultado);
 
-    printf("Digite sua altura (cm): ");
-    scanf("%f", &altura);
+    return resultado;
+}
 
+double calcula_imc(float peso, float altura) {
     altura = altura / 100;
 
-    double imc = peso / (altura * altura);
+    return peso / (altura * altura);
+}
 
+char* classifica_imc(double imc) {
     // https://abeso.org.br/wp-content/uploads/2019/12/Diretrizes-Download-Diretrizes-Brasileiras-de-Obesidade-2016.pdf
     // Tabela 04 - Página 16.
     if (imc < 18.5) {
-        printf("Você está magro.");
+        return "Você está magro.\n";
     } else if (imc < 25) {
-        printf("Você está eutrófico (peso normal).");
+        return "Você está eutrófico (peso normal).\n";
     } else if (imc < 30) {
-        printf("Você está pré-obeso.");
+        return "Você está pré-obeso.\n";
     } else if (imc < 35) {
-        printf("Você está com obesidade grau 1.");
+        return "Você está com obesidade grau 1.\n";
     } else if (imc < 40) {
-        printf("Você está com obesidade grau 2.");
+        return "Você está com obesidade grau 2.\n";
     } else {
-        printf("Você está com obesidade grave (grau 3).");
+        return "Você está com obesidade grave (grau 3).\n";
     }
+}
 
-    printf("\n");
+int main() {
+    float peso = leia_float("Digite seu peso (kg): ");
+    float altura = leia_float("Digite sua altura (cm): ");
+
+    double imc = calcula_imc(peso, altura);
+
+    char* classificacao = classifica_imc(imc);
+
+    printf(classificacao);
 
     return 0;
 }
