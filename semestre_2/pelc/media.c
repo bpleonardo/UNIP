8b3fcd08ea4579c5c675f371a7c88cc5086a1002
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2025-09-09 19:50:46 -0300

    Add media.c for grade average calculation
    Implement calculator for average of grades.

---
+++ semestre_2/pelc/media.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float leia_float(char* prompt) {
+    printf("%s", prompt);
+
+    float resultado;
+    scanf("%f", &resultado);
+
+    return resultado;
+}
+
+int main() {
+    printf("Calculadora de média.\n");
+    
+    float soma;
+    int qnt = 0;
+    float num;
+    
+    do {
+        num = leia_float("Digite a nota ou -1 para sair: ");
+        if (num > 0) {
+            soma += num;
+            qnt++;
+        }
+    } while (num > 0);
+    
+    printf("A média das %d notas é: %f.", qnt, (soma / qnt));
+}
