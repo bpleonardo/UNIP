220f7aadbe242f7386eaaa4bd25dff8a6a98422c
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2025-10-14 20:40:12 -0300

    Fazer soma

--- semestre_2/pelc/malloc.c
+++ semestre_2/pelc/malloc.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define null 0
-
 int leia_int(char* prompt);
 
 int main() {
     int t = leia_int("Insira o tamanho: ");
 
-    int *ptr = (int*) malloc(t * sizeof(int));
-    printf("%p", (void*)ptr);
+    if (t > 0) {
+        int *ptr = (int*) malloc(t * sizeof(int));
 
-    free(ptr);
-    ptr = null;
+        for (int i = 0; i < t; i++) {
+            printf("Insira o valor da posição %d: ", i);
+            scanf("%d", &ptr[i]);
+        }
+
+        int soma = 0;
+        for (int i = 0; i < t; i++) {
+            soma += ptr[i];
+        }
+
+        printf("A soma dos valores informados é %d", soma);
+
+        free(ptr);
+        ptr = NULL;
+    } else {
+        printf("O valor informado deve ser maior que 0.");
+    }
 
     return 0;
 }
