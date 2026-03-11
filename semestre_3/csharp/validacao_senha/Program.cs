4b5d32165fbc739d5f63f21293757dd79b012d27
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2026-03-11 16:58:56 -0300

    Adicionar validacao_senha

---
+++ semestre_3/csharp/validacao_senha/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace validacao_senha {
+    internal class Program {
+        static void Main() {
+            string senha_correta = "2026";
+
+            Console.WriteLine("-=-=-= Validação de senha =-=-=-");
+            while (true) {
+                Console.Write("Digite senha: ");
+                string senha = Console.ReadLine() ?? "";
+
+                if (senha == senha_correta) {
+                    Console.WriteLine("Acesso Permitido");
+                    break;
+                } else {
+                    Console.WriteLine("Senha Invalida");
+                }
+            }
+        }
+    }
+}
