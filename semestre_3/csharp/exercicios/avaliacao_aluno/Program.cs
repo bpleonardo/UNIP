6abc16b09146b0a7679dad6b4dcfe0b2c73d4735
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2026-03-13 17:23:03 -0300

    Adicionar exercicios

---
+++ semestre_3/csharp/exercicios/avaliacao_aluno/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Exercício 1 - Sistema de Avaliação de Aluno
+
+Neste exercício, você deverá desenvolver um programa em C# que receba o nome do aluno, duas
+notas e o percentual de frequência. O sistema deve calcular a média do aluno e informar sua
+situação final (Aprovado, Recuperação ou Reprovado) de acordo com as regras definidas.
+*/
+using System.Globalization;
+
+namespace avaliacao_aluno;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("-=-=-= Avaliação aluno =-=-=-");
+        string nome = ReadLine("Digite o nome do aluno: ");
+
+        double n1 = double.Parse(ReadLine("Digite a primeira nota: "), CultureInfo.InvariantCulture);
+
+        double n2 = double.Parse(ReadLine("Digite a segunda nota: "), CultureInfo.InvariantCulture);
+
+        int freq = int.Parse(ReadLine("Digite a frequência do aluno (0-100): "));
+
+        double media = (n1 + n2) / 2;
+
+        string status;
+
+        if (freq < 75 || media < 5)
+        {
+            status = "Reprovado";
+        }
+        else if (media < 6)
+        {
+            status = "Recuperação";
+        }
+        else
+        {
+            status = "Aprovado";
+        }
+
+        Console.WriteLine($"Nome do aluno: {nome}");
+        Console.WriteLine($"Média final: {media:F2}");
+        Console.WriteLine($"Frequência: {freq}");
+        Console.WriteLine($"Situação final: {status}");
+    }
+
+    static string ReadLine(string prompt)
+    {
+        Console.Write(prompt);
+        string output = "";
+
+        while (output == "")
+        {
+            output = Console.ReadLine() ?? "";
+        }
+
+        return output;
+    }
+}
---
+++ semestre_3/csharp/exercicios/contas_bancarias/Program.cs
@@ -0,0 +1,177 @@
+﻿using System.Globalization;
+
+namespace contas_bancarias;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> nomes = [];
+        List<double> saldos = [];
+
+        bool executando = true;
+
+        while (executando)
+        {
+            ExibirMenu();
+
+            if (!int.TryParse(Console.ReadLine(), out int opcao))
+            {
+                continue;
+            }
+
+            switch (opcao)
+            {
+                case 1:
+                    CadastrarConta(nomes, saldos);
+                    break;
+                case 2:
+                    ListarContas(nomes, saldos);
+                    break;
+                case 3:
+                    Depositar(nomes, saldos);
+                    break;
+                case 4:
+                    Sacar(nomes, saldos);
+                    break;
+                case 5:
+                    executando = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    static void Pausar(string? mensagem = null)
+    {
+        if (mensagem != null)
+        {
+            Console.WriteLine(mensagem);
+        }
+        Console.WriteLine("Pressione qualquer tecla para continuar.");
+        Console.ReadKey();
+    }
+
+    static void ExibirMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("------- Gerenciamento Bancário -------");
+        Console.WriteLine("1. Cadastrar conta");
+        Console.WriteLine("2. Listar contas");
+        Console.WriteLine("3. Depositar");
+        Console.WriteLine("4. Sacar");
+        Console.WriteLine("5. Sair");
+        Console.WriteLine("--------------------------------------");
+        Console.Write("> ");
+    }
+
+    static void CadastrarConta(List<string> nomes, List<double> saldos)
+    {
+        Console.Clear();
+
+        int n = nomes.Count;
+
+        if (n == 3)
+        {
+            Pausar("Limite máximo de contas atingido!");
+            return;
+        }
+
+        Console.Write($"Digite o nome do {n + 1}º títular: ");
+        string nome = Console.ReadLine() ?? "";
+        if (nome == "")
+        {
+            Pausar("Nome inválido!");
+            return;
+        }
+
+        Console.Write($"Digite seu saldo inicial: ");
+        if (!double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double saldo))
+        {
+            Pausar("Saldo inválido.");
+            return;
+        }
+
+        nomes.Add(nome);
+        saldos.Add(saldo);
+    }
+
+    static void ListarContas(List<string> nomes, List<double> saldos)
+    {
+        Console.Clear();
+        Console.WriteLine("------- Contas cadastradas -------");
+        for (int i = 0; i < nomes.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {nomes[i]} - Saldo: R${saldos[i]:F2}.");
+        }
+        Console.WriteLine("----------------------------------");
+        Pausar();
+    }
+
+    static void Depositar(List<string> nomes, List<double> saldos)
+    {
+        Console.Clear();
+
+        Console.WriteLine("------- Contas cadastradas -------");
+        for (int i = 0; i < nomes.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {nomes[i]} - Saldo: R${saldos[i]:F2}.");
+        }
+        Console.WriteLine("----------------------------------");
+
+        Console.Write("Selecione o número da conta: ");
+        if (!int.TryParse(Console.ReadLine(), out int opcao) || opcao <= 0 || opcao > nomes.Count)
+        {
+            Pausar("Conta inválida.");
+            return;
+        }
+
+        double saldoAnt = saldos[opcao - 1];
+
+        Console.Write($"Insira o valor a depositar (atual: R${saldoAnt:F2}): ");
+        if (!double.TryParse(Console.ReadLine(), out double depost) || depost < 0)
+        {
+            Pausar("Depósito inválido.");
+            return;
+        }
+
+        saldos[opcao - 1] += depost;
+    }
+
+    static void Sacar(List<string> nomes, List<double> saldos)
+    {
+        Console.Clear();
+
+        Console.WriteLine("------- Contas cadastradas -------");
+        for (int i = 0; i < nomes.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {nomes[i]} - Saldo: R${saldos[i]:F2}.");
+        }
+        Console.WriteLine("----------------------------------");
+
+        Console.Write("Selecione o número da conta: ");
+        if (!int.TryParse(Console.ReadLine(), out int opcao) || opcao <= 0 || opcao > nomes.Count)
+        {
+            Pausar("Conta inválida.");
+            return;
+        }
+
+        double saldoAnt = saldos[opcao - 1];
+
+        Console.Write($"Insira o valor a sacar (atual: R${saldoAnt:F2}): ");
+        if (!double.TryParse(Console.ReadLine(), out double saque) || saque < 0)
+        {
+            Pausar("Saque inválido.");
+            return;
+        }
+
+        if ((saldoAnt - saque) < 0)
+        {
+            Pausar("Saque deixaria o títular negativado!");
+            return;
+        }
+
+        saldos[opcao - 1] -= saque;
+    }
+}
